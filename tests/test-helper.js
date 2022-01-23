e8e1e1de21a36b73d875d5f29bd4e2da7d15f965
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-01-23 11:44:19 -0500

    Update linting to use shared config

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,9 +1,10 @@
-import Application from 'wordle/app';
-import config from 'wordle/config/environment';
-import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+
+import Application from 'wordle/app';
+import config from 'wordle/config/environment';
 
 setApplication(Application.create(config.APP));
 
